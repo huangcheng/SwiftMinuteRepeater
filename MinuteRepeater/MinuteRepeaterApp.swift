77172fe2a24980445b8b6a45ccae2eccbed9001f
HUANG Cheng <cheng@duck.com>
2025-12-24 15:39:46 +0800

    Initial Commit

---
+++ MinuteRepeater/MinuteRepeaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinuteRepeaterApp.swift
+//  MinuteRepeater
+//
+//  Created by Cheng Huang on 2025-12-24.
+//
+
+import SwiftUI
+
+@main
+struct MinuteRepeaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MinuteRepeaterTests/MinuteRepeaterTests.swift
@@ -0,0 +1,17 @@
+//
+//  MinuteRepeaterTests.swift
+//  MinuteRepeaterTests
+//
+//  Created by Cheng Huang on 2025-12-24.
+//
+
+import Testing
+@testable import MinuteRepeater
+
+struct MinuteRepeaterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
